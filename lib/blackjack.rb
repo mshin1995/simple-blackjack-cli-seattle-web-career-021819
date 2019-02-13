d56a1500fd931459ecff08e3ff7889f218954f0c
Matthew Shin <mshin1995@gmail.com>
2019-02-13 05:00:33 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -33,7 +33,7 @@ def hit?(card_total)
   get_user_input
   if get_user_input = 'h'
     card_total += deal_card
-  elsif get_user_input ='s'
+  elsif get_user_input = 's'
     card_total
   else
     invalid_command
